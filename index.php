2df45519f8236acb9c0e2c7e077f273f00bb42e6
George McMullen <gmcmullen@mail.greenriver.edu>
2021-05-05 03:55:39 -0400

    Added composer, view, and index to route to view

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//require autoload file
+require_once("vendor/autoload.php");
+
+// Start a session
+session_start();
+
+//create an instance of the base class
+$f3 = Base::instance();
+
+//set fat-free debugging
+$f3->set('DEBUG', 3);
+
+//Define a default route (home page)
+$f3->route('GET /', function(){
+    // render home.html
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+//run fat free
+$f3->run();
