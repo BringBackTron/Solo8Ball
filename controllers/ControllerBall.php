5f364b1db1faf95422c9c30318ee833997b46927
George McMullen <gmcmullen@mail.greenriver.edu>
2021-06-15 23:12:57 -0400

    Removed debugging and added documentation for controllers

--- controllers/ControllerBall.php
+++ controllers/ControllerBall.php
@@ -1,9 +1,5 @@
 <?php
 
-//turn on error reporting
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
 /**
  * Class controller
  * @author Jean-Kenneth "Jake" Antonio
@@ -29,10 +25,13 @@ class ControllerBall
         //Display the home page
         $view = new Template();
         echo $view->render('views/navbar.html');
-        $this->updateLogOut();
         echo $view->render('views/home.html');
+        $this->updateLogOut();
     }
 
+    /**
+     * Redirects to the game page
+     */
     function game()
     {
         //display game page
@@ -42,6 +41,9 @@ class ControllerBall
         $this->updateLogOut();
     }
 
+    /**
+     * Redirects to the login page
+     */
     function login()
     {
         //Makes fields sticky and sets default value
@@ -79,9 +81,6 @@ class ControllerBall
                 $pulledCreds = $GLOBALS['dataLayer']->pullCredentials($username);
                 var_dump($pulledCreds);
                 $userid = $pulledCreds['user_id'];
-                echo "<script>console.log('$userid')</script>";
-                echo "<script>console.log('passed log id')</script>";
-
                 $hashedPassword = password_hash($pulledCreds['password'], PASSWORD_DEFAULT);
 
                 $isTrue = password_verify($password, $hashedPassword);
@@ -90,7 +89,7 @@ class ControllerBall
                     $_SESSION['loggedin'] = true;
                     $_SESSION['user_id'] = $pulledCreds['user_id'];
                     $_SESSION['username'] = $pulledCreds['username'];
-                    header("Location: /328/Solo8Ball");
+                    header("Location: /Solo8Ball");
                 }
             }
 
@@ -102,6 +101,9 @@ class ControllerBall
         $this->updateLogOut();
     }
 
+    /**
+     * Redirects to the simulator page
+     */
     function sim()
     {
         $this->_f3->clear('submitted');
@@ -110,8 +112,6 @@ class ControllerBall
         if (isset($_SESSION['loggedin'])) {
             $user = $GLOBALS['dataLayer']->pullPlayerData($_SESSION['user_id']);
             $userid = $_SESSION['user_id'];
-            echo "<script>console.log('pulling player data line 115')</script>";
-            echo "<script>console.log('$userid')</script>";
             $this->_f3->set('user', $user);
             $player->setUserID($user['user_ID']);
             $player->setTotalGames($user['total_scores']);
@@ -135,7 +135,6 @@ class ControllerBall
             $this->_f3->set('scoreSticky', 0);
         }
 
-        echo "<script>console.log('past sticky line 141')</script>";
 
         //send scores
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -145,11 +144,8 @@ class ControllerBall
                 $player->setTime($_POST['time']);
                 $player->setShots($_POST['shots']);
                 $player->setScore($_POST['score']);
-                echo "<script>console.log('before sfd saving scores line 151')</script>";
                 $scoreID = $GLOBALS['dataLayer']->saveScore($player);
-                echo "<script>console.log('after saving data line 153')</script>";
                 $GLOBALS['dataLayer']->updatePlayerData($player);
-                echo "<script>console.log('after update data line 155')</script>";
                 $this->_f3->set('submitted', true);
 
             } else {
@@ -157,14 +153,11 @@ class ControllerBall
             }
         }
 
-        echo "<script>console.log('past sending scores line 162')</script>";
         //display game page
         $view = new Template();
 
         echo $view->render('views/navbar.html');
-        echo "<script>console.log('past navbar line 163')</script>";
         echo $view->render('views/gameSim.html');
-        echo "<script>console.log('not logged in line 165')</script>";
         $this->updateLogOut();
         $this->updateSimButton();
 
@@ -175,19 +168,9 @@ class ControllerBall
 
     }
 
-    function simtest()
-    {
-        //display game page
-        $view = new Template();
-
-        echo $view->render('views/navbar.html');
-        var_dump($_SESSION);
-        echo $view->render('views/gameSim.html');
-        $this->updateLogOut();
-        $this->updateSimButton();
-
-    }
-
+    /**
+     * Redirects to the leaderboard page
+     */
     function leaderboard()
     {
         $tableData = $GLOBALS['dataLayer']->pullLeaderboardData();
@@ -199,16 +182,9 @@ class ControllerBall
         $this->updateLogOut();
     }
 
-    function about()
-    {
-        //display about page
-        $view = new Template();
-        //include("views/navbar.html");
-        echo $view->render('views/navbar.html');
-        echo $view->render('views/about.html');
-        $this->updateLogOut();
-    }
-
+    /**
+     * Updates the login/logout button depending on current session
+     */
     function updateLogOut()
     {
 
@@ -217,46 +193,49 @@ class ControllerBall
             echo "<script>document.getElementById('loginButton').innerHTML = '<b>$username</b>'</script>";
             echo '<script>document.getElementById("loginButton").innerHTML += " - Logout"</script>';
             echo '<script>document.getElementById("loginButton").href = "logout"</script>';
-            echo '<script>console.log("logged in, log out button is on")</script>';
 
             //$this->_f3->set('loginBtn', "Logout");
             //$this->_f3->set('loginBtnRef', "logout");
         } else {
             echo '<script>document.getElementById("loginButton").innerHTML = "Login"</script>';
             echo '<script>document.getElementById("loginButton").href = "login"</script>';
-            echo '<script>console.log("logged out, log in button is on")</script>';
             //$this->_f3->set('loginBtn', "Login");
             //$this->_f3->set('loginBtnRef', "login");
         }
 
     }
 
+    /**
+    * Updates the login/logout button depending on current session
+    */
     function updateSimButton()
     {
 
         if(!isset($_SESSION['loggedin'])) {
             echo '<script>document.getElementById("btn-submit-score").hidden = true</script>';
             echo '<script>document.getElementById("disabledText").innerText = "Please log in to submit"</script>';
-            echo '<script>console.log("logged out, submit score button is off")</script>';
-            echo "<script>console.log('not logged in line 238')</script>";
         } else {
             echo '<script>document.getElementsByClassName("disabledText").value = ""</script>';
-            echo '<script>console.log("logged in, submit score button is on")</script>';
         }
     }
 
+    /**
+     * Logs out of the current session, redirects to home page
+     */
     function logOut()
     {
         session_destroy();
         session_start();
-        header("Location: /328/Solo8Ball");
+        header("Location: /Solo8Ball");
     }
 
+    /**
+     * Updates the simulator button to show a message when a score is submitted
+     */
     function buttonSubmission()
     {
            echo '<script>document.getElementById("btn-submit-score").hidden = true</script>';
            echo '<script>document.getElementById("disabledText").innerText = "Your submission was counted!"</script>';
-        echo "<script>console.log('btn submission line 255')</script>";
     }
 
 
